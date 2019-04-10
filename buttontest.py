960b4058fe8631c7e4f76dbede2f689ffe743082
eddieredmann3 <eddieredmann3@gmail.com>
2019-04-10 18:24:23 -0500

    Update buttontest.py

--- buttontest.py
+++ buttontest.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.wPi)
 GPIO.setup(switches, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(leds, GPIO.OUT)
 
-leds = []
+leds = [2, 3, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18]
 switches = [14, 13, 12, 10, 11, 4, 5, 6, 12, 13, 16, 17]
 
 def play():
@@ -16,4 +16,5 @@ def play():
       while GPIO.input(switches[i]):
         val = i
         pressed = True
+        GPIO.OUT(leds[i])
       print("Yay")
