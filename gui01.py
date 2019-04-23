9a95555e443caf87c832050cf4dc0b2f13906cdb
eddieredmann3 <eddieredmann3@gmail.com>
2019-04-23 17:32:31 -0500

    Update gui01.py

--- gui01.py
+++ gui01.py
@@ -3,10 +3,13 @@ from Tkinter import *
 class App(Frame):
   def __init__(self, master):
     Frame.__init__(self, master)
+    self.currentscreen = "title"
   
   def setBackground(self):
     #this function should put the images in the proper places
-    pass
+    App.img = None
+    if self.currentscreen == "title":
+      App.img = PhotoImage(file="Title.gif")
   
   def createButtons(self):
     #this function should create the buttons for doing various things
@@ -15,4 +18,8 @@ class App(Frame):
 
   def setUpGUI(self):
     #this function compiles the GUI
+    self.pack(fill=BOTH, expand=1) 
+  
+  def process(self):
+    #this should make the buttons do things
     pass
