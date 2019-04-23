fb3ad488583fd8e07fd8a8739c5b1ea350c1a8b2
eddieredmann3 <eddieredmann3@gmail.com>
2019-04-23 17:56:16 -0500

    Update gui01.py

--- gui01.py
+++ gui01.py
@@ -3,6 +3,7 @@ from Tkinter import *
 class App(Frame):
   def __init__(self, master):
     Frame.__init__(self, master)
+    master.attributes("-fullscreen", True)
     self.currentscreen = "title"
     self.img = None
   
@@ -10,7 +11,7 @@ class App(Frame):
     #this function should put the images in the proper places
     if self.currentscreen == "title":
       self.img = PhotoImage(file="Title.gif")
-  
+      
   def createButtons(self):
     #this function should create the buttons for doing various things
     #like the "play" and various lesson buttons
@@ -19,9 +20,16 @@ class App(Frame):
   def setUpGUI(self):
     #this function compiles the GUI
     self.pack(fill=BOTH, expand=1)
-    if self.currentscreen = "title":
-      self.display = Label(self, text = "", anchor = E, bg = "white", height = 2, font=("TexGyreAdventor", 50))
-  
+    bgimg = self.img
+    App.image = Label(self, width="WIDTH", image=bgimg)
+    App.image.pack(side=MIDDLE, fill=BOTH)
+    App.image.pack_propagate(False)
+      
   def process(self):
     #this should make the buttons do things
     pass
+
+window = Tk()
+window.title("Piano Lessons")
+app = App(window)
+window.mainloop()
