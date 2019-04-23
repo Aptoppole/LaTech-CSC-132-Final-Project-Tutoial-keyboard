04f27a2efe4a03f915ac7683d36c140eb64cdfc5
eddieredmann3 <eddieredmann3@gmail.com>
2019-04-23 17:10:45 -0500

    Update gui01.py

--- gui01.py
+++ gui01.py
@@ -1,1 +1,18 @@
 from Tkinter import *
+
+class App(Frame):
+  def __init__(self, master):
+    Frame.__init__(self, master)
+  
+  def setBackground(self):
+    #this function should put the images in the proper places
+    pass
+  
+  def createButtons(self):
+    #this function should create the buttons for doing various things
+    #like the "play" and various lesson buttons
+    pass
+
+  def setUpGUI(self):
+    #this function compiles the GUI
+    pass
