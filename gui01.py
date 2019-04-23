1bc3e795f82dcf5e728c003b03993ea46b5064d7
eddieredmann3 <eddieredmann3@gmail.com>
2019-04-23 17:39:36 -0500

    Update gui01.py

--- gui01.py
+++ gui01.py
@@ -4,12 +4,12 @@ class App(Frame):
   def __init__(self, master):
     Frame.__init__(self, master)
     self.currentscreen = "title"
+    self.img = None
   
   def setBackground(self):
     #this function should put the images in the proper places
-    App.img = None
     if self.currentscreen == "title":
-      App.img = PhotoImage(file="Title.gif")
+      self.img = PhotoImage(file="Title.gif")
   
   def createButtons(self):
     #this function should create the buttons for doing various things
@@ -18,7 +18,9 @@ class App(Frame):
 
   def setUpGUI(self):
     #this function compiles the GUI
-    self.pack(fill=BOTH, expand=1) 
+    self.pack(fill=BOTH, expand=1)
+    if self.currentscreen = "title":
+      self.display = Label(self, text = "", anchor = E, bg = "white", height = 2, font=("TexGyreAdventor", 50))
   
   def process(self):
     #this should make the buttons do things
