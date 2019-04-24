8b530f126c16d017ec71468430a5c017c27fea89
eddieredmann3 <eddieredmann3@gmail.com>
2019-04-24 16:34:51 -0500

    Update gui01.py

--- gui01.py
+++ gui01.py
@@ -11,6 +11,7 @@ class App(Frame):
   def setBackground(self):
     #this function should put the images in the proper places
     self.pack(fill=BOTH, expand=1)
+    backButton = Button(self, bg="lightgrey", text="Back", command = lambda: self.process("Back"))
     if self.currentscreen == "title":
       global creditButton, startButton, image
       self.img = PhotoImage(file="Title.gif")
@@ -32,10 +33,11 @@ class App(Frame):
       text.pack(fill=BOTH, expand=1)
       text_frame.pack(side=TOP, fill=BOTH)
       text.insert(END, "Contributors: \n\n Jonah Fitzgerald, \n\n Alex Petty, \n\n Eddie Redmann")
-      backButton = Button(self, bg="lightgrey", text="Back", command = lambda: self.process("Back"))
       backButton.pack(side=BOTTOM, fill=X)
 
       text.config(state=DISABLED)
+    elif self.currentscreen == "start":
+      backButton.pack(side=BOTTOM, fill=X)
   def process(self, prompt):
     #this should make the buttons do things
     if prompt == "Credits":
@@ -52,10 +54,14 @@ class App(Frame):
       image.pack_forget()
     elif prompt == "Back":
       global backButton, text, text_frame
+      if self.currentscreen == "credit":
+        backButton.pack_forget()
+        text.pack_forget()
+        text_frame.pack_forget()
+        self.currentscreen = "title"
+      elif self.currentscreen == "start":
+        backButton.pack_forget()
       self.currentscreen = "title"
-      backButton.pack_forget()
-      text.pack_forget()
-      text_frame.pack_forget()
     else:
       print "You piece of shit. What did you do?"
     self.setBackground()
@@ -67,5 +73,3 @@ window.title("Piano Lessons")
 window.geometry("{}x{}".format(WIDTH, HEIGHT))
 app = App(window)
 window.mainloop()
-
-
