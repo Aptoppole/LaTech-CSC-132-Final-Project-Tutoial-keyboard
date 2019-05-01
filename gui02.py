ab80596c7343d4d4ae7d50a556d6ec8064b32ad4
eddieredmann3 <eddieredmann3@gmail.com>
2019-05-01 07:58:27 -0500

    Update gui02.py

--- gui02.py
+++ gui02.py
@@ -7,17 +7,17 @@ class App(Frame):
     self.currentscreen = "title"
     self.img = None
     self.setBackground()
-    self.creditButton = Button(self, bg="lightgray", text="Credits", command = lambda: self.process("Credits"))
-    self.startButton = Button(self, bg="lightgray", text="Start", command = lambda: self.process("Start"))
-    self.backButton = Button(self, bg="lightgrey", text="Back", command = lambda: self.process("Back"))
   
   def setBackground(self):
     #this function should put the images in the proper places
     self.pack(fill=BOTH, expand=1)
     if self.currentscreen == "title":
-      self.img = PhotoImage(file="Title.gif")
+      global image
       self.creditButton = Button(self, bg="lightgray", text="Credits", command = lambda: self.process("Credits"))
       self.startButton = Button(self, bg="lightgray", text="Start", command = lambda: self.process("Start"))
+      self.backButton = Button(self, bg="lightgrey", text="Back", command = lambda: self.process("Back"))
+ 
+      self.img = PhotoImage(file="Title.gif")
       self.creditButton.pack(side=BOTTOM, fill=X)
       self.startButton.pack(side=BOTTOM, fill=X)
 
@@ -25,6 +25,7 @@ class App(Frame):
       image.pack(side=TOP, fill=BOTH)
       
     elif self.currentscreen == "credit":
+      global text, text_frame
       self.img = None
       
       text_frame = Frame(self, width=WIDTH)
@@ -37,17 +38,17 @@ class App(Frame):
 
       text.config(state=DISABLED)
     elif self.currentscreen == "start":
-      for row in range(9):
+      for row in range(5):
         Grid.rowconfigure(self, row, weight=1)
-      for col in range(3):
+      for col in range(5):
         Grid.columnconfigure(self, col, weight=1)
-      button1 = Button(self, bg = "lightgrey", text = "Lesson 1: Mary Had a Little Lamb", command = lambda: self.process("marylamb"))
-      button1.grid(row = 1, column = 1, sticky = N+E+W+S)
-      button2 = Button(self, bg = "lightgrey", text = "Lesson 2: Ode to Joy", command = lambda: self.process("odetojoy"))
-      button2.grid(row = 3, column = 1, sticky = N+E+W+S)
-      button3 = Button(self, bg = "lightgrey", text = "Lesson 3: The Third One", command = lambda: self.process("thethirdone"))
-      button3.grid(row = 5, column = 1, sticky = N+E+W+S)
-      self.backButton.grid(row=7, column=1, sticky = N+E+W+S)
+      self.button1 = Button(self, bg = "lightgrey", text = "Lesson 1: Mary Had a Little Lamb", command = lambda: self.process("marylamb"))
+      self.button1.grid(row = 1, column = 1, sticky = N+E+W+S)
+      self.button2 = Button(self, bg = "lightgrey", text = "Lesson 2: Ode to Joy", command = lambda: self.process("odetojoy"))
+      self.button2.grid(row = 1, column = 3, sticky = N+E+W+S)
+      self.button3 = Button(self, bg = "lightgrey", text = "Lesson 3: Freeplay", command = lambda: self.process("freeplay"))
+      self.button3.grid(row = 3, column = 1, sticky = N+E+W+S)
+      self.backButton.grid(row=3, column=3, sticky = N+E+W+S)
       
   def process(self, prompt):
     #this should make the buttons do things
@@ -56,34 +57,39 @@ class App(Frame):
       self.currentscreen = "credit"
       self.creditButton.pack_forget()
       self.startButton.pack_forget()
+      self.backButton.pack_forget()
       image.pack_forget()
     elif prompt == "Start":
-      global creditButton, startButton, image
+      global image
       self.currentscreen = "start"
       self.creditButton.pack_forget()
-      startButton.pack_forget()
-      self.image.pack_forget()
+      self.startButton.pack_forget()
+      image.pack_forget()
     elif prompt == "Back":
       global text, text_frame
       if self.currentscreen == "credit":
-        self.backButton.pack_forget()
-        text.pack_forget()
         text_frame.pack_forget()
         self.currentscreen = "title"
+        self.backButton.pack_forget()
       elif self.currentscreen == "title":
-        pass
+        self.backButton.pack_forget()
       elif self.currentscreen == "start":
-        pass
-      self.currentscreen = "title"
+        items = self.master.grid_slaves()
+        self.button1.grid_forget()
+        self.button2.grid_forget()
+        self.button3.grid_forget()
+        self.backButton.grid_forget()
+        self.currentscreen = "title"
     elif prompt == "marylamb":
       pass
     elif prompt == "odetojoy":
       pass
-    elif prompt == "thethirdone":
+    elif prompt == "freeplay":
       pass
     else:
       print "You piece of shit. What did you do?"
     self.setBackground()
+    
 class Lesson(object):
     def __init__(self, seq):
         self.seq = seq
